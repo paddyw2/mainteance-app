b4b0ef0bbdc6a6293b5385eadb593914e42d8f86
Paddy <p.withams@gmail.com>
2018-04-03 10:32:56 -0600

    Add test code for local mysql connection

--- handlers/car_handler.py
+++ handlers/car_handler.py
@@ -1,6 +1,17 @@
+import mysql.connector
+
 class car_handler:
 
   def connect_db(self):
+    """
+    You can test db connection using this code
+    Make sure the mysql server is running and replace
+    test_env with a database you created.
+    cnx = mysql.connector.connect(user='root', password='',
+                              host='127.0.0.1',
+                              database='test_env')
+    cnx.close()
+    """
     return True
 
   def select_query(self, user_input):
