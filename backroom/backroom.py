1d3a7f78c330903511f60294574d325e36835f96
Paddy <p.withams@gmail.com>
2018-04-12 16:25:38 -0600

    Fix customer assignment form bug

--- backroom/backroom.py
+++ backroom/backroom.py
@@ -5,7 +5,7 @@ class backroom:
       optional_values = ""
       license_no = ""
     else:
-      optional_values = ", license_no"
+      optional_values = ", assigned_to"
       license_no = ", " + str(assigned_id)
     query_string = "insert into backroom(event_id" + optional_values + ") values("+str(event_id) + license_no+");"
     print(query_string)
@@ -17,7 +17,7 @@ class backroom:
       optional_values = ""
       license_no = ""
     else:
-      license_no = ", license_no=" + str(assigned_id)
+      license_no = ", assigned_to=" + str(assigned_id)
     query_string = "update backroom set event_id="+str(event_id) + license_no + " where event_id="+str(event_id)+";"
     print(query_string)
     return query_string
--- pos/pos.py
+++ pos/pos.py
@@ -6,7 +6,7 @@ class pos:
       license_no = ""
     else:
       optional_values = ", license_no"
-      license_no = ", " + str(assigned_id)
+      license_no = ", \"" + str(assigned_id) +"\""
     query_string = "insert into pos(event_id" + optional_values + ") values("+str(event_id) + license_no+");"
     print(query_string)
     return query_string
@@ -17,7 +17,7 @@ class pos:
       optional_values = ""
       license_no = ""
     else:
-      license_no = ", license_no=" + str(assigned_id)
+      license_no = ", license_no=\"" + str(assigned_id) + "\""
     query_string = "update pos set event_id="+str(event_id) + license_no + " where event_id="+str(event_id)+";"
     print(query_string)
     return query_string
