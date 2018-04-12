b0819b41a1c0790cf1e57699ec76211219de6c6a
Paddy <p.withams@gmail.com>
2018-04-12 00:19:52 -0600

    Add edit/update support for all events

--- repair/repair.py
+++ repair/repair.py
@@ -20,3 +20,21 @@ class repair:
     print(query_string)
     return query_string
 
+  def update_repair(post_values, backroom_id):
+    est_finish_date = post_values['est_finish_date']
+    description = post_values['description']
+    print("D: "+description)
+    parts_list = post_values['parts_list']
+    if(est_finish_date != "" or est_finish_date == "None"):
+      est_finish_date = ",est_finish_date=\""+est_finish_date+"\""
+    if(description != ""):
+      description = ",description=\""+description+"\""
+    if(parts_list != ""):
+      parts_list = ",parts_list=\""+parts_list+"\""
+
+    # query
+
+    query_string = "update repair set backroom_id="+str(backroom_id)+est_finish_date+description+parts_list+" where backroom_id="+str(backroom_id)+";"
+    print(query_string)
+    return query_string
+
